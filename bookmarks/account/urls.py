0d4eb6fe5776d63947748e719a288ad1e82b371e
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-16 17:49:49 +0530

    Trying the logout .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,16 +1,14 @@
 # custom url for the account
 from django.urls import path
-# importing the auth views   
 from django.contrib.auth import views as auth_views
-
-#importing views
 
 from . import views
 
 # route
-urlpatterns =[
-   # path('login/',views.user_login,name='login'),
-   path('login/',auth_views.LoginView.as_view(),name="login"),
-   path('logout/',auth_views.LogoutView.as_view(),name="logout"),
-   path('',views.dashboard,name="dashboard"),
+urlpatterns = [
+    # previous login url
+    # path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
