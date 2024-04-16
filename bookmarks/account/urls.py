36d2b62ef851f8f9cf36a6ca573840ef795c2df6
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-16 11:20:55 +0530

    Changing the views for the django login view or logout view .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,7 @@
 # custom url for the account
 from django.urls import path
+# importing the auth views   
+from django.contrib.auth import views as auth_views
 
 #importing views
 
@@ -7,5 +9,7 @@ from . import views
 
 # route
 urlpatterns =[
-    path('login/',views.user_login,name='login'),
+   # path('login/',views.user_login,name='login'),
+   path('login/',auth_views.LoginView.as_view(),name="login"),
+   path('logout/',auth_views.LogoutView.as_view(),name="logout"),
 ]
