da68785d6a410d37a14d7b654cd6795238dd7160
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-17 11:49:43 +0530

    Adding the password reset urls for the template .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -17,4 +17,13 @@ urlpatterns = [
     path('password-change/done',
           auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
+    # reset password
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),name='password-reset_complete'),
 ]
