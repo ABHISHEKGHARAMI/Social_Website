bbcaa75877c47ae07f29ddc1ae6031a50fb822aa
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-17 15:23:07 +0530

    mod .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('password-reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('password-reset/complete/',
-         auth_views.PasswordResetCompleteView.as_view(),name='password-reset_complete'),
+         auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     
     
     path('', views.dashboard, name='dashboard'),
