40fdb3010fc2b9b9a9216dce5a39a263f36115fe
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-17 15:41:19 +0530

    mod .

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-
+from django.contrib.auth.models import User
 
 # form for th login 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+    
