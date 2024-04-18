809f350499df68143f200e3b9fdc8df214f459f3
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-18 16:36:10 +0530

    Adding the profile .

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -6,6 +6,6 @@ from .models import Profile
 
 
 @admin.register(Profile)
-class ProfileAdmin(models.ModelAdmin):
+class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user','date_of_birth','photo']
     raw_id_fields = ['user']
