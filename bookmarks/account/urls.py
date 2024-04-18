087fec229b54bf78d0946396dedeb144c341f25c
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-18 12:40:00 +0530

    Adding to the urls of the webapp .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
     
     path('',include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    
+    path('register/',views.register,name='register'),
 ]
