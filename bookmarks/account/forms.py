7af4ee60dc1794d6a5e70da86230256fed81b8de
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-18 12:27:39 +0530

    Adding the form for the registration .

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -7,3 +7,26 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     
+    
+# user registration form
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+    
+    
+    # creating the meta class
+    class Meta:
+        model = User
+        fields = ['username','first_name','email']
+        
+        
+    # checking passwords
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("passwords don\'t match")
+        return cd['password2']
+    
