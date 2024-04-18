9d92aab6bdc0beb57e8472c0d63c396d9fc38937
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-18 15:24:35 +0530

    Register model for the admin .

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(models.ModelAdmin):
+    list_display = ['user','date_of_birth','photo']
+    raw_id_fields = ['user']
