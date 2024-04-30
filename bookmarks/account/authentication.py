1bfb80683e7627e6974c4c0a55d5ea824ed23749
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-30 17:46:32 +0530

    Adding all the work done .

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,4 +1,5 @@
 # email based user auth technique
+from account.models import Profile
 from django.contrib.auth.models import User
 
 
@@ -20,3 +21,7 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+        
+
+def create_profile(backend, user, *args, **kwargs):
+    Profile.objects.get_or_create(user=user)
