74bdeb7e8ad1a61609ed6e9f415150ff1fe99a3c
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-01 14:09:09 +0530

    Adding the user like image in the field .

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -20,6 +20,9 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                       related_name='images_liked',
+                                       blank=True)
     
     
     #  meta class
