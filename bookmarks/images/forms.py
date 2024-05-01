7fcd4b396af166aaef56e713fd18e86593123fed
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-01 21:08:56 +0530

    Adding the imagefrom save image functionality .

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Image
+import requests
+from django.utils.text import slugify
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -12,8 +14,26 @@ class ImageCreateForm(forms.ModelForm):
         
     def clean_url(self):
         url = self.cleaned_data['url']
-        valid_extentions = ['jpg','jpeg','png']
-        extention = url.rsplit('.',1)[1].lower()
-        if extention not in valid_extentions:
-            raise forms.ValidationError('The url does not match with valid image extention .')
+        valid_extensions = ['jpg','jpeg','png']
+        extension = url.rsplit('.',1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The url does not match with valid image extension .')
         return url
+    
+    def save(self,force_insert = False,
+             force_update = False,
+             commit = True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.',1)[1].lower()
+        image_name = f'{name}.{extension}'
+        # download image
+        response = requests.get(image_url)
+        image.image.save(image_url,
+                         ContentFile(response.content),
+                         save=False)
+        if commit:
+            image.save()
+        return image
+        
