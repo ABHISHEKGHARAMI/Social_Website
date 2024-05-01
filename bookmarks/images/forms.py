57be9c646be614c01f829bc67a4e8cc9263a1f1a
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-01 14:33:41 +0530

    Adding the clean_url for method for the ImageCreateForm .

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -9,3 +9,11 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url' : forms.HiddenInput,
         }
+        
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extentions = ['jpg','jpeg','png']
+        extention = url.rsplit('.',1)[1].lower()
+        if extention not in valid_extentions:
+            raise forms.ValidationError('The url does not match with valid image extention .')
+        return url
